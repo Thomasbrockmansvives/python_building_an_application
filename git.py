7bd8229588fc069b3adfcc53e66996d5ea0121c4
Thomasbrockmansvives <thomas.brockmans@student.vives.be>
2025-10-22 19:54:33 +0200

    added config to gitignore

--- git.py
+++ git.py
@@ -12,3 +12,6 @@ Created on 22-10-2025
 print("===================================================")
 print("This is just to see the working of git in python.")
 print("===================================================")
+
+print("")
+print("the config file should be ignored")
